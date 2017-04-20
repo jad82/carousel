bd727995d8e4222af5a62470730965f5eab92cb5
jad82 <jan.drzymala@live.com>
2017-04-20 15:16:20 +0200

    karuzela z przyciskami

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+$(function() { 
+	console.log ('DOM gotowy, można działać dalej');
+	var carouselList = $("#carousel ul");
+	function showNext () {
+		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+	};
+	function moveFirstSlide () {
+		var firstItem = carouselList.find("li:first"); 
+		var lastItem = carouselList.find("li:last");
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft:0});
+	};
+
+	function showPrevious () {
+		moveLastSlide();
+		carouselList.animate({'marginLeft': -400}, 500, moveLastSlide);
+	};
+
+	function moveLastSlide () {
+		var firstItem = carouselList.find("li:first"); 
+		var lastItem = carouselList.find("li:last");
+		firstItem.before(lastItem);
+		carouselList.css({marginLeft:-400});
+	};
+
+	setInterval(showNext, 3000);
+	var nextButton = $("#next");
+  	var prevButton = $("#prev");
+	
+	nextButton.click(function (){
+		showNext();
+	});
+	prevButton.click (function (){
+		showPrevious();
+	});
+});
